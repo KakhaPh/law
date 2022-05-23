8f7c89d8970e4b001740818dba542b7e6bfc9e47
KakhaPh_07 <kk.phutkaradze@gmail.com>
2022-05-23 22:22:36 +0400

    try hard

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <h2>this is about page</h2>
-       
-
+    <div class="about_banner_div">
+        <img src="{{ asset('storage/Images/page-title-background.jpg') }}" class="about_title">
+        <h1 class="about_banner_title">ჩვენს შესახებ</h1>
+        <h3 class="about_banner_title_under">ევროპის უნივერსიტეტის სამართლის ინსტიტუტი წესდება</h3>
     </div>
     
 @endsection
