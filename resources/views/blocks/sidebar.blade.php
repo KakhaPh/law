56d0696055156bcdc2f48c68733ec1ee26818aea
kakhaPh <kakha.putkaradze@eu.edu.ge>
2022-06-15 16:37:24 +0400

    news + sidebar

---
+++ resources/views/blocks/sidebar.blade.php
@@ -0,0 +1,62 @@
+<div class="news__sidebar">
+    <form method="GET" class="searchform">
+        <div class="inp__box">
+            <input class="search_type" autocomplete="off" placeholder="Type keywords...">
+            <button class="search_submit" type="submit"><i class="fas fa-search"></i></button>
+        </div>
+    </form>
+    <div class="news_sidebar__textWidget">
+        <h4>Text Widget</h4>
+        <p>Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida at eget metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+    </div>
+
+    <div class="news__sidebar__practiceWidget">
+        <h4>Practice Widget</h4>
+        <div class="news__sidebar__practiceWidget__item">
+            <img src="{{asset('storage/Images/acc_img1.jpg')}}" width='65' height='65' alt="">
+            <div class="news__sidebar__practiceWidget__item-info">
+                <h5>5 წელი ერთად</h5>
+                <a href="#">Learn more <i class="fas fa-arrow-right"></i></a>
+            </div>
+        </div>
+
+        <div class="news__sidebar__practiceWidget__item">
+            <img src="{{asset('storage/Images/acc_img2.jpg')}}" width='65' height='65' alt="">
+            <div class="news__sidebar__practiceWidget__item-info">
+                <h5>2019 წლის ანგარიში</h5>
+                <a href="#">Learn more <i class="fas fa-arrow-right"></i></a>
+            </div>
+        </div>
+
+        <div class="news__sidebar__practiceWidget__item">
+            <img src="{{asset('storage/Images/acc_img3.jpg')}}" width='65' height='65' alt="">
+            <div class="news__sidebar__practiceWidget__item-info">
+                <h5>2018 წლის ანგარიში</h5>
+                <a href="#">Learn more <i class="fas fa-arrow-right"></i></a>
+            </div>
+        </div>
+
+        <div class="news__sidebar__practiceWidget__item">
+            <img src="{{asset('storage/Images/acc_img4.jpg')}}" width='65' height='65' alt="">
+            <div class="news__sidebar__practiceWidget__item-info">
+                <h5>2015-2017 წლების ანგარიში</h5>
+                <a href="#">Learn more <i class="fas fa-arrow-right"></i></a>
+            </div>
+        </div>
+
+        <br>
+        <div class="news__sidebar__comments">
+            <h4>ბოლო კომენტარები</h4><br><br>
+        </div>
+
+        <div class="news__sidebar__tags">
+            <h4>Tag Cloud</h4>
+            <div class="news__sidebar__tags_main">
+                @foreach($tags as $tag)
+                <div class="news__sidebar__tags__item">
+                    <a href="tags/{{ $tag->tags_id }}">{{ $tag->tags_name }}</a>
+                </div>
+                @endforeach
+            </div>
+        </div>
+</div>
--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -3,95 +3,38 @@
 @section('content')
     <div style="background-image: url('{{ asset('storage/Images/page-title-background.jpg') }}')" class="news_banner_div">
         <h1 class="news_banner_title">{{$menu_info->name}}</h1>
-        <h3 class="news_banner_title_under">{{$menu_info->body}}</h3>
+        <h5 class="news_banner_title_under">{{$menu_info->body}}</h5>
     </div>
 
-      <div class="news__container">
-          <div class="news_items">
-                @foreach($news as $news_item)
-                <div class="news_item">
-                    <div class="news_item_img">
-                        <img  src="{{ asset( $news_item->news_image) }}" alt="">
-                    </div>
-                    <div class="news_item_info">
-                        <h3>{{ $news_item->news_title }}</h3>
-                        <div class="news_item_subInfo">
-                            <p>
-                                 <i class="fas fa-clock"></i>
-                           {{ 
-                            // date convertation to day month year
-                            date('j F Y', strtotime($news_item->news_created_at))
+    <div class="news__container">
+        <div class="news_items">
+            @foreach($news as $news_item)
+            <div class="news_item">
+                <div class="news_item_img">
+                    <a href="{{ route('news.show',$news_item->news_id) }}"><img  src="{{ asset( $news_item->news_image) }}"></a>
+                </div>
+                <div class="news_item_info">
+                    <h5>{{ $news_item->news_title }}</h5>
+                    <div class="news_item_subInfo">
+                        <p>
+                            <i class="fas fa-clock"></i>
+                        {{ 
 
-                            }}</p>
-                            <p class="user_info"> <i class="fas fa-user"></i> {{ $news_item->name }} </p>
-                        </div>
-                        <p>{{ $news_item->news_about }}</p>
-                    </div>
-                    <a href=" {{ route('news.show',$news_item->news_id) }}" class="news_btn-link">Read More</a>
-                </div>
-                @endforeach
-          </div>
-          <div class="news__sidebar">
-              <div class="news__sidebar__input">
-                    <input type="text" placeholder="Type keywords...">
-              </div>
-              <div class="news_sidebar__textWidget">
-                  <h3>Text Widget</h3>
-                    <p>Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida at eget metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                    </p>
-              </div>
-              <div class="news__sidebar__practiceWidget">
-                <h3>Practice Widget</h3>
-                <div class="news__sidebar__practiceWidget__item">
-                    <img src="{{asset('storage/Images/acc_img1.jpg')}}" width='65' height='65' alt="">
-                    <div class="news__sidebar__practiceWidget__item-info">
-                        <h3>5 წელი ერთად</h3>
-                        <a href="#">Learn more</a>
-                    </div>
-                </div>
+                        // date convertation to day month year
+                        date('j F Y', strtotime($news_item->news_created_at))
 
-                <div class="news__sidebar__practiceWidget__item">
-                    <img src="{{asset('storage/Images/acc_img1.jpg')}}" width='65' height='65' alt="">
-                    <div class="news__sidebar__practiceWidget__item-info">
-                        <h3>5 წელი ერთად</h3>
-                        <a href="#">Learn more</a>
+                        }}</p>
+                        <p class="user_info"> <i class="fas fa-user"></i> {{ $news_item->name }} </p>
                     </div>
+                    <p>{{ $news_item->news_about }}</p>
                 </div>
-
-                <div class="news__sidebar__practiceWidget__item">
-                    <img src="{{asset('storage/Images/acc_img1.jpg')}}" width='65' height='65' alt="">
-                    <div class="news__sidebar__practiceWidget__item-info">
-                        <h3>5 წელი ერთად</h3>
-                        <a href="#">Learn more</a>
-                    </div>
-                </div>
-
-                 <div class="news__sidebar__practiceWidget__item">
-                    <img src="{{asset('storage/Images/acc_img1.jpg')}}" width='65' height='65' alt="">
-                    <div class="news__sidebar__practiceWidget__item-info">
-                        <h3>5 წელი ერთად</h3>
-                        <a href="#">Learn more</a>
-                    </div>
-                </div>
+                <a href="{{ route('news.show',$news_item->news_id) }}" class="news_btn-link">Read More</a>
             </div>
-
-            <div class="news__sidebar__comments">
-                <h3>ბოლო კომენტარები</h3>
-                {{-- ... --}}
-            </div>
-            
-            <div class="news__sidebar__tags">
-                <h4>Tag Cloud</h4>
-            <div class="news__sidebar__tags_main">
-                @foreach($tags as $tag)
-                <div class="news__sidebar__tags__item">
-                    <a href="tags/{{ $tag->tags_id }}">{{ $tag->tags_name }}</a>
-                 </div>
-                @endforeach
-            </div>
+            @endforeach
         </div>
-          </div>
-      </div>
+        {{-- Sidebar --}}
+        @include('blocks.sidebar')
+    </div>
     
 @endsection
 
